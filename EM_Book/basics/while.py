36387e4c5dab4c871422ec395e11c84ab9220edb
Alex Jeronimo <jeronimoalex@gmial.com>
2022-03-18 16:09:38 +0200

    add ned data

--- EM_Book/basics/while.py
+++ EM_Book/basics/while.py
@@ -7,14 +7,41 @@
 #         print(f"I'd love to go to {city.title()}!")
 
 
-unconfirmed_users = ['Alice', 'Bob', 'Candace']
-confirmed_users = []
+# unconfirmed_users = ['Alice', 'Bob', 'Candace']
+# confirmed_users = []
+#
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+#
+#     print(f"Verifying user : {current_user.title()}")
+#     confirmed_users.append(current_user)
+# print("\nThe following users have been confirmed:")
+# for confirmed_user in confirmed_users[::-1]:
+#     print(confirmed_user.title())
 
-while unconfirmed_users:
-    current_user = unconfirmed_users.pop()
 
-    print(f"Verifying user : {current_user.title()}")
-    confirmed_users.append(current_user)
-print("\nThe following users have been confirmed:")
-for confirmed_user in confirmed_users:
-    print(confirmed_user.title())
+# pets = ['dog', 'cat', 'parrot', 'goldfish', 'cat', 'dog', 'rabbit']
+# print(pets)
+#
+# while 'cat' in pets:
+#     pets.remove('cat')
+# print(pets)
+
+responses = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name? ")
+    response = input("Which mountainn would you like to climb someday? ")
+
+    responses[name] = response
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+
+    if repeat == 'no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name , response in responses.items():
+    print(f"{name} would like to climb {response}.")
+
