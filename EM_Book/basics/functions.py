030ceaebac0665535fd02309ae0f298134bed697
Alex Jeronimo <jeronimoalex@gmial.com>
2022-03-24 18:48:07 +0200

    add ned data

--- EM_Book/basics/functions.py
+++ EM_Book/basics/functions.py
@@ -1,6 +1,22 @@
-def greet_user(user):
-    print(f"Hello, {user.title()}!")
+# def greet_user(user):
+#     print(f"Hello, {user.title()}!")
+#
+#
+# greet_user("Alex")
 
+# def describe_pet(animal_type, pet_name):
+#    print(f"\nI have a {animal_type}.")
+#    print(f"My {animal_type}'s name is {pet_name.title()}.")
+#
+#
+# describe_pet('cat', 'stupid')
 
-greet_user("Alex")
+# def build_person(first_name, last_name, age=None):
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+#
+# musician = build_person('jimi', 'hendrix', age=27)
+# print(musician)
 
--- EM_Book/home work/hw8.py
+++ EM_Book/home work/hw8.py
@@ -14,3 +14,51 @@
 #
 #
 # favorite_book(book)
+
+# 8.3
+# def make_shirt(size, text):
+#     print(f"Shirt size is {size}")
+#     print(f"Slogan is '{text}'")
+#
+# make_shirt('XXL', 'make love')
+# make_shirt(size='XL', text='make love')
+
+# 8.4
+# def make_shirt(size='L', text='I love Python'):
+#     print(f"Shirt size is {size}")
+#     print(f"Slogan is '{text}'")
+#
+# make_shirt()
+# make_shirt(size='XL', text='make love')
+
+# 8.5
+# def describe_city(city, country):
+#     print(f"{city.title()} is in {country}")
+#
+# describe_city('Kyiv', 'Ukraine')
+# describe_city('London', 'Great Britain')
+# describe_city('Washingtom', 'USA')
+
+# 8.6
+
+# 8.7
+
+# 8.8
+
+# 8.9
+
+# 8.10
+
+# 8.11
+
+# 8.12
+
+# 8.13
+
+# 8.14
+
+# 8.15
+
+# 8.16
+
+# 8.17
