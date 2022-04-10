ce04ed747790c51946c2d15992ea32a38d765863
Alex Jeronimo <jeronimoalex@gmial.com>
2022-04-10 13:53:48 +0300

    new info

---
+++ EM_Book/basics/for.py
@@ -0,0 +1,34 @@
+# tl = [('a', 'b', 'c'), (1, 5.4, 90), ('k', 32, 45)]
+# for element in tl:
+#     print(element)
+#
+# print("")
+#
+# for el1, el2, el3 in tl:
+#     print(el1, el2, el3)
+
+# dl = {'key1': 'value1', 'key2': 43, 'key3': 'something'}
+# for item in dl:
+#     print(item)
+#
+# for item in dl.items():
+#     print(item)
+#
+# for item in dl.keys():
+#     print(item)
+#
+# for item in dl.values():
+#     print(item)
+
+
+# for x in range(11):
+#     print('\U0001f600' * x)
+
+
+# for x in range(11):
+#     count = 0
+#     emoticons = ''
+#     while count <= x:
+#         emoticons += '\U0001f600'
+#         count += 1
+#     print(emoticons)
--- EM_Book/basics/while.py
+++ EM_Book/basics/while.py
@@ -27,21 +27,25 @@
 #     pets.remove('cat')
 # print(pets)
 
-responses = {}
+# responses = {}
+#
+# polling_active = True
+#
+# while polling_active:
+#     name = input("\nWhat is your name? ")
+#     response = input("Which mountainn would you like to climb someday? ")
+#
+#     responses[name] = response
+#     repeat = input("Would you like to let another person respond? (yes/no) ")
+#
+#     if repeat == 'no':
+#         polling_active = False
+#
+# print("\n--- Poll Results ---")
+# for name , response in responses.items():
+#     print(f"{name} would like to climb {response}.")
 
-polling_active = True
-
-while polling_active:
-    name = input("\nWhat is your name? ")
-    response = input("Which mountainn would you like to climb someday? ")
-
-    responses[name] = response
-    repeat = input("Would you like to let another person respond? (yes/no) ")
-
-    if repeat == 'no':
-        polling_active = False
-
-print("\n--- Poll Results ---")
-for name , response in responses.items():
-    print(f"{name} would like to climb {response}.")
-
+x = 2
+while x >= 1:
+    print(x)
+    x -= 1
