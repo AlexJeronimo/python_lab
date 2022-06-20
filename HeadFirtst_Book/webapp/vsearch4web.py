6fb4e5753a3c58c924136c29d7988b0aaef82e07
Alex Jeronimo <jeronimoalex@gmail.com>
2022-06-20 22:06:40 +0300

    added try/except

--- HeadFirtst_Book/webapp/vsearch4web.py
+++ HeadFirtst_Book/webapp/vsearch4web.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, escape, session
+from flask import Flask, render_template, request, redirect, escape, session, copy_current_request_context
 from vsearch import search_for_letters
+from threading import Thread
+from time import sleep
 # import mysql.connector
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
@@ -12,48 +14,52 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                           'database': 'vsearchlogDB', }
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    """Log details of the web request and the results."""
-    # with open('vsearch.log', 'a') as log:
-    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Log details of the web request and the results."""
 
-    # conn = mysql.connector.connect(**dbconfig)
-    # cursor = conn.cursor()
+        # with open('vsearch.log', 'a') as log:
+        #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
-    try:
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """insert into log
-                          (phrase, letters, ip, browser_string, results)
-                          values
-                          (%s, %s, %s, %s, %s)"""
-            cursor.execute(_SQL, (req.form['phrase'],
-                                  req.form['letters'],
-                                  req.remote_addr,
-                                  str(req.user_agent),
-                                  # 'Firefox',
-                                  res,))
-    except ConnectionError as err:
-        print('Is your database switched on? Error:', str(err))
-    except CredentialsError as err:
-        print('Uer-is/Password issue. Error:', str(err))
-    except SQLError as err:
-        print('Is your query correct? Error:', str(err))
-    except Exception as err:
-        print('Something went wrong: ', str(err))
+        # conn = mysql.connector.connect(**dbconfig)
+        # cursor = conn.cursor()
+
+        try:
+            sleep(15)
+            with UseDatabase(app.config['dbconfig']) as cursor:
+                _SQL = """insert into log
+                              (phrase, letters, ip, browser_string, results)
+                              values
+                              (%s, %s, %s, %s, %s)"""
+                cursor.execute(_SQL, (req.form['phrase'],
+                                      req.form['letters'],
+                                      req.remote_addr,
+                                      str(req.user_agent),
+                                      # 'Firefox',
+                                      res,))
+        except ConnectionError as err:
+            print('Is your database switched on? Error:', str(err))
+        except CredentialsError as err:
+            print('Uer-is/Password issue. Error:', str(err))
+        except SQLError as err:
+            print('Is your query correct? Error:', str(err))
+        except Exception as err:
+            print('Something went wrong: ', str(err))
 
     # conn.commit()
     # cursor.close()
     # conn.close()
 
-
-@app.route('/search4', methods=['POST'])
-def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search_for_letters(phrase, letters))
     try:
-        log_request(request, results)
+        # log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('****** Logging failed with this error: ', str(err))
     return render_template('results.html',
